3b19ebfda7b5dc212c8fe0a718e310a6d8c150d1
Yanulio <yagshulepa@edu.hse.ru>
2023-11-18 17:51:45 +0300

    feat: add repo for users last used commands

---
+++ innBot/Data/LastCommandRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+
+namespace innBot.Data;
+
+public class LastCommandRepository
+{
+    private ConcurrentDictionary<long, string> lastCommands = new ConcurrentDictionary<long, string>();
+
+    public void SaveCommand(string command, long senderId)
+    {
+        lastCommands.AddOrUpdate(senderId, command, (key, oldValue) => command);
+    }
+
+    public string GetLastCommand(long senderId)
+    {
+        if (lastCommands.TryGetValue(senderId, out string lastCommand))
+        {
+            return lastCommand;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
